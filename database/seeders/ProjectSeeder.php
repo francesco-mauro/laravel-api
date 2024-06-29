fb29ae13438792013bca6299fe54689dd91b9aea
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-29 19:09:03 +0200

    add: seeder and model

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Project::create([
+            'title' => 'Example Project 1',
+            'description' => 'Description for project dfsfas'
+        ]);
+
+        Project::create([
+            'title' => 'Example Project 2',
+            'description' => 'Description for project gffrrg'
+        ]);
     }
-}
+};
